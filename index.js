ab213f209468c65b54c0ca31401e7d3c3a17c8fe
xxxdrewwerdxxx <88953175+xxxdrewwerdxxx@users.noreply.github.com>
2022-03-02 16:46:38 -0500

    New Game

---
+++ index.js
@@ -0,0 +1,11 @@
+// REFER TO THE README ON WHAT CORE TO USE!! https://github.com/ethanaobrien/emulatorjs-chrome-extension/blob/main/README.md
+
+EJS_player = '#game';
+EJS_gameUrl = 'game.zip'; // Url to Game rom
+EJS_core = 'nes';
+EJS_pathtodata = 'data/'; //path to all of the wasm and js files.
+
+EJS_player = '#game';
+EJS_gameUrl = 'Cooking Mama 2 - Dinner with Friends (USA).nds'; // Url to Game rom
+EJS_core = 'nds';
+EJS_pathtodata = 'data/'; //path to all of the wasm and js files
